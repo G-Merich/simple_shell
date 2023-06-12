038b50624280a1ff1d8b6648cb2899dd2b4c060d
Gloria Merit Ogudu <gmerit2030@gmail.com>
2023-06-12 01:07:36 -0700

    Correction

---
+++ helper_1.c
@@ -0,0 +1,51 @@
+#include "shell.h"
+
+/**
+ * free the fields needed each loop
+ */
+void free_recurrent_data(data_of_program *data)
+{
+	if (data->tokens)
+		free_array_of_pointers(data->tokens);
+	if (data->input_line)
+		free(data->input_line);
+	if (data->command_name)
+		free(data->command_name);
+
+	data->input_line = NULL;
+	data->command_name = NULL;
+	data->tokens = NULL;
+}
+
+/**
+ * free all field of the data
+ */
+
+void free_all_data(data_of_program *data)
+{
+	if (data->file_descriptor != 0)
+	{
+		if (close(data->file_descriptor))
+			perror(data->program_name);
+	}
+	free_recurrent_data(data);
+	free_array_of_pointers(data->env);
+	free_array_of_pointers(data->alias_list);
+}
+
+/**
+ * free each pointer of an array of pointers and the
+ */
+void free_array_of_pointers(char **array)
+{
+	int i;
+
+	if (array != NULL)
+	{
+		for (i = 0; array[i]; i++)
+			free(array[i]);
+
+		free(array);
+		array = NULL;
+	}
+}
